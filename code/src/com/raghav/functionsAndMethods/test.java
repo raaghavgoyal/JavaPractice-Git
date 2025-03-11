72d9eb5146e4311b0dc4a0f67c6d55e6b56193b1
Raghav <raaghavgoyal@gmail.com>
2025-03-11 22:32:11 +0530

    Functions/Methods in Java ammended

---
+++ code/src/com/raghav/functionsAndMethods/test.java
@@ -0,0 +1,9 @@
+package com.raghav.functionsAndMethods;
+class Main {
+    public static void main(String args[]){
+        final int i=10;
+        //i = 20;
+        System.out.println("Hello, this is a program for functions and methods.");
+        System.out.println(i);
+    }
+}
