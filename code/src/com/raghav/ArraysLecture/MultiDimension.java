dbb4912ab29152d80c3a1cd739b7358f90a47570
Raghav <raaghavgoyal@gmail.com>
2025-03-18 06:17:33 +0530

    Arryas part 2

--- code/src/com/raghav/ArraysLecture/MultiDimension.java
+++ code/src/com/raghav/ArraysLecture/MultiDimension.java
@@ -1,6 +1,7 @@
 package com.raghav.ArraysLecture;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class MultiDimension {
     public static void main(String[] args) {
@@ -13,14 +14,41 @@ public class MultiDimension {
         4 5 6
         7 8 9
         * */
+        Scanner sc = new Scanner(System.in);
         //int[][] arr = new int[3][];// *adding the rows is mandatory not the columns
-        int [][] arr = {
-                {1,2,3},
-                {4,5,6,666,77,88},
-                {7,8,9}
-        };
-        System.out.println(Arrays.toString(arr[0]));
+        /*int [][] arr = {
+                {1,2,3},            //0th index
+                {4,5,6,666,77,88},  //1st index
+                {7,8,9}             //2nd index-> arr2D[2] = {7,8,9}
+        };*/
+        //input
+        int [][]arr = new int[3][3];
+        System.out.println(arr.length);//number of rows
+        for(int row = 0; row <arr.length ; row++){
+            for(int col = 0; col<arr[row].length ; col++){
+                arr[row][col] = sc.nextInt();
+            }
+        }
+        //output
+        /*for(int row = 0; row <arr.length ; row++){
+            for(int col = 0; col<arr[row].length ; col++){
+                System.out.print(arr[row][col]+" ");
+            }
+            System.out.println();
+        }*/
+
+        /*for(int row = 0; row <arr.length ; row++){
+            System.out.println(Arrays.toString(arr[row]));
+        }*/
+
+        for(int[] r : arr){
+            System.out.println(Arrays.toString(r));
+        }
+        //System.out.println(Arrays.toString(arr[0]));
         //just a comment
+
+
+
 
     }
 }
