b6f3d24537832ca36feca54d293db4eb03712121
Raghav <raaghavgoyal@gmail.com>
2025-04-01 12:32:24 +0530

    find element in a sorted matrix

--- code/src/com/raghav/BiarySearch/RowColMatrix.java
+++ code/src/com/raghav/BiarySearch/RowColMatrix.java
@@ -4,17 +4,17 @@ import java.util.Arrays;
 
 public class RowColMatrix {
     public static void main(String[] args) {
-        int [] [] arr = {
+        int [] [] arr = {{1,3}};/*{
                 {10, 20, 30, 40},
                 {15, 25, 35, 45},
                 {28, 29, 37, 49},
                 {33, 34, 38, 50}
-        };
-        System.out.println(Arrays.toString(search(arr,39)));
+        };*/
+        System.out.println(Arrays.toString(search(arr,3)));
     }
     static int [] search(int [][] matrix, int target){
         int r = 0;
-        int c = matrix.length -1;
+        int c = matrix[0].length -1;
         while(r< matrix.length && c>= 0){
             if(matrix[r][c] == target){
                 return new int[]{r,c};
