55c9fad4d1447c3376ed1763d20c0bea9d7ceb75
Raghav <raaghavgoyal@gmail.com>
2025-06-13 11:31:01 +0530

    cyclic sort algo implement

--- code/src/com/raghav/Sorting/CyclicSort.java
+++ code/src/com/raghav/Sorting/CyclicSort.java
@@ -6,7 +6,7 @@ import static com.raghav.Sorting.SelectionSort.swap;
 
 public class CyclicSort {
     public static void main(String[] args) {
-        int[] arr = {3,4,2,1,5,6,7};
+        int[] arr = {7,6,5,4,3,2,1};
         System.out.println(Arrays.toString(arr));
         cyclic(arr);
         System.out.println(Arrays.toString(arr));
