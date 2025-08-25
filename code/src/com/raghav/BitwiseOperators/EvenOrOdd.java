00a2dc33204c405d40e3596ae030ad7280c7eba7
Raghav <raaghavgoyal@gmail.com>
2025-08-25 15:21:16 +0530

    isOdd using bitwise and with 1

---
+++ code/src/com/raghav/BitwiseOperators/EvenOrOdd.java
@@ -0,0 +1,12 @@
+package com.raghav.BitwiseOperators;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int n = 67;
+        System.out.println(isOdd(n));
+    }
+
+    private static boolean isOdd(int n) {
+        return (n & 1) == 1;
+    }
+}
