8182bd78661810bc3e232eb8be50e6b1ddf94b2f
Raghav <raaghavgoyal@gmail.com>
2025-09-08 08:58:11 +0530

    Sum of first n numbers using recursionn

---
+++ code/src/com/raghav/Recursion2Easy/Sum.java
@@ -0,0 +1,14 @@
+package com.raghav.Recursion2Easy;
+
+public class Sum {
+    public static void main(String[] args) {
+        int ans = sum(5);
+        System.out.println(ans);
+    }
+    static int sum(int n){
+        if(n <= 1){
+            return 1;
+        }
+        return n + sum(n-1);
+    }
+}
