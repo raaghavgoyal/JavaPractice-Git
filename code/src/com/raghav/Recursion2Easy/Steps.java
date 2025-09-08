84c039bf88a443a94eaa9b4503275738a6a6a7ed
Raghav <raaghavgoyal@gmail.com>
2025-09-08 15:17:18 +0530

    CountZeros and Steps using recursion

---
+++ code/src/com/raghav/Recursion2Easy/Steps.java
@@ -0,0 +1,22 @@
+package com.raghav.Recursion2Easy;
+
+//https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+public class Steps {
+    public static void main(String[] args) {
+
+    }
+
+    public int numberOfSteps(int num) {
+        return helper(num, 0);
+    }
+    static int helper(int num, int c){
+        if(num == 0){
+            return c;
+        }
+        if(num % 2 == 0){
+            return helper(num/2, c+1);
+        }else{
+            return helper(num-1, c+1);
+        }
+    }
+}
