cfcc75f30f0976fdf68250e42311218c7e33e27a
Raghav <raaghavgoyal@gmail.com>
2025-09-18 22:04:39 +0530

    refactorrrr

--- code/src/Test/sql.java
+++ code/src/Test/sql.java
@@ -1,7 +1,7 @@
 package Test;
 
 public class sql {
-    //SQL questionssss
+    //SQL questionsssss
     //list all employess (emp name  => dept name)
     // of all departments for which employe age> 20 and department band > 10000
 
